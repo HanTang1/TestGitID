7805e62c86c6e303d204138aa513b00fcefeaf56
tanghan <tanghan@jucaicat.com>
2017-08-29 16:44:49 +0800

    fist demo

--- src/main/java/com/hantang/Demo.java
+++ src/main/java/com/hantang/Demo.java
@@ -4,6 +4,6 @@ import org.omg.Messaging.SYNC_WITH_TRANSPORT;
 
 public class Demo {
     public static void main(String[] args){
-        System.out.println("aaaaa");
+        System.out.println("Hello aaaaa");
     }
 }
